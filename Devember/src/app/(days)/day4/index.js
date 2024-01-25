fc9049b7aafc9d15c30d40c55213a0fe5a2ab83c
KANNAN4307 <kannan4307@gmail.com>
2024-01-25 17:11:10 +0530

    Lottie splash screen , splash not included

--- Devember/src/app/(days)/day4/index.js
+++ Devember/src/app/(days)/day4/index.js
@@ -1,11 +1,17 @@
+import { Link, Stack } from 'expo-router';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const Day4 = () => {
   return (
     <View style={styles.container}>
-      <Text>day 4 </Text>
-    </View>
+    <Stack.Screen options={{title:'Day 4'}}/>
+    <Link asChild href={'day4/SplashAnimation'}>
+      <Button 
+       title='Go to Splash screen page'
+      />
+   </Link>
+</View>
   );
 };
 
